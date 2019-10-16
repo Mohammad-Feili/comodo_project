495ed240ca5f9309534d54f409148d4a7d0cf285
mohammad <mfeili@gmail.com>
2019-10-16 16:09:39 +0330

    Email changed

--- comodo/settings.py
+++ comodo/settings.py
@@ -136,14 +136,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-# mail config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
-
-
 try:
     from .local_settings import *
 except ImportError:
